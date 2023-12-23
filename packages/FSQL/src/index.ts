9ecb3b07f6880d8cc5fff8ed307688523c0fd102
Andy Wilson <andy.wilson1234@gmail.com>
2023-12-23 22:43:45 +0000

    Update index.ts

--- packages/FSQL/src/index.ts
+++ packages/FSQL/src/index.ts
@@ -1,5 +1,53 @@
-import create from './create';
+import { default as createFn } from './create';
+import { default as readFn } from './read';
+import { default as createTableFn } from './createTable';
 
-export {
-  create
+class DB {
+  public folder: string;
+  public database: string;
+  public table: string;
+
+  constructor(database: string, folder?: string = '.') {
+    this.database = database;
+    this.folder = folder;
+  }
+
+  private async function migrate() {
+    const migrationsDir = `${process.cwd()}/${process.env.migrations ?? "migrations"}`;
+
+    const lastMigration = await this.read("migrations").orderBy("date", "DESC").columns("name").limit(1).get();
+
+    if (typeof lastMigration === "string") {
+      if (lastMigration.includes("does not exist")) {
+        await this.createTable("migrations", {
+          name: {
+            type: "string",
+            lnth: 255,
+            unique: true,
+          }
+        })
+      }
+    }
+  }
+
+  private function setTable(table: string) {
+    this.table = table;
+  }
+
+  private async function createTable(table: string, schema: object) {
+    setTable(table);
+    return createTableFn(this, schema);
+  }
+
+  public async function read<Data>(table: string): Promise<Data[] | string> {
+    setTable(table);
+    return readFn(this);
+  }
+
+  public async function create<Data>(table: string, data: Data | Data[]): Promise<Data[] | string> {
+    setTable(table);
+    return createFn(this);
+  }
 }
+
+export default DB;
