1b4791035f8cef59d000505a9a09c3788da6eabc
Andy Wilson <andy.wilson1234@gmail.com>
2023-12-23 22:59:52 +0000

    Create createTable.ts

---
+++ packages/FSQL/src/createTable.ts
@@ -0,0 +1,19 @@
+class createTable {
+  public table: string;
+  public folder: string;
+  public database: string;
+  public schema: object;
+  
+  constructor(_this: any, schema: object) {
+    this.table = _this.table;
+    this.folder = _this.folder;
+    this.database = _this.database;
+    this.schema = schema;
+  }
+
+  public async function create() {
+    return true;
+  }
+}
+
+export default createTable;
