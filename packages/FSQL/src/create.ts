c0cf72752625c0fc3e63f7eaf9c71cf1638f966d
Andy Wilson <andy.wilson1234@gmail.com>
2023-12-23 20:44:11 +0000

    Update create.ts

--- packages/FSQL/src/create.ts
+++ packages/FSQL/src/create.ts
@@ -1,1 +1,35 @@
+import { v4 as uuid } from 'uuid';
 
+type createPropsDefault = {
+  [x: string]: string | number | boolean;
+}
+
+export interface createPropsSingle extends createPropsDefault {
+  table: string;
+  _id?: string;
+}
+
+export interface createPropsMulti extends createPropsDefault {
+  table: string;
+  data: Array<{
+    _id?: string;
+  }>;
+}
+
+export type createProps = createPropsSingle | createPropsMulti;
+
+const create = ({ _id, data = undefined }: createProps) => {
+  if (data) {
+    return [{
+      id: uuid()
+    }];
+  }
+
+  const id = _id ?? uuid();
+
+  return {
+    id
+  }
+}
+
+export default create;
