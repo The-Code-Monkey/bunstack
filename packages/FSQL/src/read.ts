002796556f56fcf7ba62ff22e79505a8e9ba2bb1
Andy Wilson <andy.wilson1234@gmail.com>
2023-12-23 22:53:49 +0000

    Update read.ts

--- packages/FSQL/src/read.ts
+++ packages/FSQL/src/read.ts
@@ -1,1 +1,29 @@
+class read {
+  public table: string;
+  public folder: string;
+  public database: string;
+  
+  constructor(_this: any) {
+    this.table = _this.table;
+    this.folder = _this.folder;
+    this.database = _this.database;
+  }
 
+  public function orderBy(column: string, order: 'ASC' | 'DESC') {
+    return this;
+  }
+
+  public function columns(...args: string[]) {
+    return this;
+  }
+
+  public function limit(number: number) {
+    return this;
+  }
+
+  public async function get() {
+    return {};
+  }
+}
+
+export default read;
