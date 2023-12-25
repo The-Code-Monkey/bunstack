0f4efb3fa85afd76d6799a69bd90dfaf4f78fcb8
Andy Wilson <andy.wilson1234@gmail.com>
2023-12-25 06:53:49 +0000

    Update update.ts

--- packages/FSQL/src/update.ts
+++ packages/FSQL/src/update.ts
@@ -1,1 +1,14 @@
+class update<Data> {
+  public table: string;
+  public folder: string;
+  public database: string;
 
+  constructor(_this: any) {
+    this.table = _this.table;
+    this.folder = _this.folder;
+    this.database = _this.database;
+  }
+
+}
+
+export default update;
