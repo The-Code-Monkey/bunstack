17b4ef2444c5ad1a12ac840883c3701e27f98393
Andy Wilson <andy.wilson1234@gmail.com>
2023-12-26 13:33:35 +0000

    Update read.ts

--- packages/FSQL/src/read.ts
+++ packages/FSQL/src/read.ts
@@ -147,17 +147,18 @@ class read<Data> {
       results = results.slice(0, this.limitNumber);
     }
 
-   // if (this.columnsToGet) {
-    //  results = results.map(result => {
-    //    const newResult = {};
-   //     
-   //     for (const column in this.columnsToGet) {
-    //      newResult[column] = result[column];
-   //     }
-//
-      //  return newResult;
-   //   })
- //   }
+    if (this.columnsToGet[0] !== "*") {
+      results = results.map(result => {
+        const newResult = {};
+        
+        for (const column in this.columnsToGet) {
+          const key = this.columnsToGet[column];
+          newResult[key] = result[key];
+        }
+
+        return newResult;
+      })
+    }
 
     return results;
   }
