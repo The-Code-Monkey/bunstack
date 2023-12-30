d1b45e5390e3c05e2fa95c819349f463d0d36c0c
Andy Wilson <andy.wilson1234@gmail.com>
2023-12-30 21:28:10 +0000

    Update read.ts

--- packages/FSQL/src/read.ts
+++ packages/FSQL/src/read.ts
@@ -174,7 +174,7 @@ class read<Data extends object> {
 
           results = newResults;
         } else {
-          const newResults = Data[] = [];
+          const newResults: Data[] = [];
           for (let i = 0; i < results.length; i++) {
             const result = results[i];
             const newReadInstance = new read({ table: `${this.table}-${join[0]}`, folder: this.folder, database: this.database } as DB).where(`${this.table}Id`, '=', result.id);
