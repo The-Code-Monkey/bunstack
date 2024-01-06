b822fd8567de1b76e9afd0318a45127704d861b3
Andy Wilson <andy@builtbypixel.com>
2024-01-06 11:12:04 +0000

    fix: import direct from fs

--- packages/FSQL/src/delete.ts
+++ packages/FSQL/src/delete.ts
@@ -1,5 +1,5 @@
 // Import the unlink function from the 'fs/promises' module in Node.js. This function is used to delete files.
-import { unlink } from "node:fs/promises";
+import fs from "fs/promises";
 
 // Import the default export from the current directory, which is presumably the DB class.
 import DB from '.';
@@ -45,7 +45,7 @@ class deleteFn {
     }
 
     // Delete the file.
-    await unlink(path);
+    await fs.unlink(path);
 
     // Return a success message.
     return `Successfully deleted the entry`;
